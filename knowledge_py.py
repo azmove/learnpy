e1154fd824c4593109183494791541b2bfe0dfc6
azmove <lovebtcs@gmail.com>
2018-03-15 09:08:40 -0700

    modified:   knowledge_py.py

--- knowledge_py.py
+++ knowledge_py.py
@@ -1,12 +1,13 @@
-'''
+#用来获取api文档的python请求示例
+#可能由于网络原因本案例无法返回结果 但适用于python3.6
+
 #coding=utf-8
 import socket,urllib.request
 import json
 import ast
 timeout = 5
-'''
 
-''socket.setdefaulttimeout(timeout)
+socket.setdefaulttimeout(timeout)
 chaper_url = 'https://api.huobi.pro/market/depth?symbol=ethusdt&type=step1'
 # 定义一个网站头UA
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'}
@@ -18,5 +19,5 @@ print(content)
 json_content = json.loads(content)
 tick =json_content['tick']
 bids=tick['bids']
-#print(bids)
-'''
+print(bids)
+
